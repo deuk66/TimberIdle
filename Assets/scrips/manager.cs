2a62f89cd0dcaa22c930e43cb298c78194d27849
deuk66 <benny50305@gmail.com>
2026-09-08 15:49:03 +0800

    v0.21
    第二次專題課

--- Assets/scrips/manager.cs
+++ Assets/scrips/manager.cs
@@ -2,26 +2,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
+public struct space{
+    public GameObject tree;
+    public Vector3 position;
+};
 public class manager : MonoBehaviour
 {
     public GameObject tree;
     public int map_size=9;
     public int tree_quantity=36;
-    struct space{
-        public GameObject tree;
-        public Vector3 position;
-    };
-    space [,] map;
     
+    public GameObject tree_top;
     void Start(){
-        map=new space[map_size,map_size];
-        GameObject tree_top=new GameObject{};
+        manager_data.map=new space[map_size,map_size];
 
         for(int i = 0; i < map_size; i++)
         {
             for(int j = 0; j < map_size; j++)
             {
-                map[i,j].position=new Vector3(i-4,j-4,-0.01f);
+                manager_data.map[i,j].position=new Vector3(i-4,j-4,-0.01f);
             }
         }
 
@@ -31,10 +30,10 @@ public class manager : MonoBehaviour
             {
                 int i=Random.Range(0,map_size);
                 int j=Random.Range(0,map_size);
-                if (!map[i, j].tree)
+                if (!manager_data.map[i, j].tree)
                 {
-                    manager_data.trees.Add(Instantiate(tree,map[i,j].tree.transform.position,tree.transform.rotation,tree_top.transform));
-                    map[i,j].tree=manager_data.trees[_];
+                    manager_data.trees.Add(Instantiate(tree,manager_data.map[i,j].position,tree.transform.rotation,tree_top.transform));
+                    manager_data.map[i,j].tree=manager_data.trees[_];
                     break;
                 }
             }
@@ -45,7 +44,9 @@ public class manager : MonoBehaviour
     }
 
 }
+
 public static class manager_data
 {
+    public static space [,] map;
     public static List<GameObject> trees=new List<GameObject>{};
 }
